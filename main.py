e39245f95692e24dfe7f4f824284e0be6413b8dd
Igor-Aleksandrov <igoruale@gmail.com>
2021-10-17 18:15:19 +0500

    test auto build

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 
 class MortgageCalc1App(App):
     def build(self):
-        return Label(text="Hello, Calc")
+        return Label(text="Hello, Calc1")
 
 
 MortgageCalc1App().run()
